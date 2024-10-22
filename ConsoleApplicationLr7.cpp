b5850e9d349a92f45a3414b4981b85f9095c95e3
Kirill Abraimov <kirillabraimov@gmail.com>
2024-10-22 21:30:07 +0300

    first commit

---
+++ ConsoleApplicationLr7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "math.h"
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Введіть довжину в см - ";
+    double a;
+    cin >> a;
+    cout << "Довжина в м - ";
+    cout << int(a) / 100;
+}
